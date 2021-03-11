8bff3e94784efe0a1659a632868a66992ae6feae
Barengific <75044077+Barengific@users.noreply.github.com>
2021-03-11 17:21:36 +0000

    Update PeerStarts.java
    Updated to listening for clients

--- J/src/main/java/Net/PeerStarts.java
+++ J/src/main/java/Net/PeerStarts.java
@@ -1,5 +1,6 @@
 package Net;
 
+import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -8,6 +9,21 @@ public class PeerStarts {
     static final int port = 1234;
     static ServerSocket ssocket;
     static Socket socket;
+
+    public static void main(String[] args) throws IOException, IOException {
+        ssocket = new ServerSocket(port);
+
+        int player = 1;
+        while (true) {
+            System.out.println("listening" + player);
+            socket = ssocket.accept();
+            if (player == 1) {
+                ServThreader thread = new ServThreader(socket, player);
+                thread.start();
+                player = 2;
+            }
+        }
+    }
 
 
 }
