395b7a86fdb2a76443efe60e8157c42c2f3b8681
Barengific <75044077+Barengific@users.noreply.github.com>
2021-03-11 17:08:57 +0000

    Create PeerStarts.java
    Server quick impl

---
+++ J/src/main/java/Net/PeerStarts.java
@@ -0,0 +1,13 @@
+package Net;
+
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class PeerStarts {
+    //server variables creation
+    static final int port = 1234;
+    static ServerSocket ssocket;
+    static Socket socket;
+
+
+}
